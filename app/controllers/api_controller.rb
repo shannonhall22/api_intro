66596d1f90e469d278cf4e3b6ced7b131dba8855
shannonhall22 <shannonhall22@gmail.com>
2015-04-20 19:11:57 -0500

    Updated for lon, lat, wind and temps

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -12,5 +12,13 @@ class ApiController < ApplicationController
     parsed_info = JSON.parse(initial_info)
     @result = parsed_info["weather"][0]["main"]
     @icon = parsed_info["weather"][0]["icon"]
+    @latitude = parsed_info["coord"]["lat"]
+    @longitude = parsed_info["coord"]["lon"]
+    @wind_speed = parsed_info["wind"]["speed"]
+    @wind_direction = parsed_info["wind"]["deg"]
+    max_temp = parsed_info["main"]["temp_max"]
+    @max_temperature = (max_temp - 273.15)*1.8 + 32
+    min_temp = parsed_info["main"]["temp_min"]
+    @min_temperature = (min_temp - 273.15)*1.8 + 32
   end
 end
